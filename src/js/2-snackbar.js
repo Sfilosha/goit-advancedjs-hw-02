530c19d1b86f5dc6c744f21c2dcf730fd52963ca
Sfilosha <sfilosha@gmail.com>
2024-12-15 21:46:14 +0200

    init file

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,10 @@
+// Описаний у документації https://marcelodolza.github.io/iziToast/
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+`✅ Fulfilled promise in ${delay}ms`
+`❌ Rejected promise in ${delay}ms`
+
+
+
